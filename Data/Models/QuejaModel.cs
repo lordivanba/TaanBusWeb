c2aec69cbbcda706b32c4b1cd816b72c384bc573
Ivan Canto <ivanbamail@gmail.com>
2022-03-19 18:58:35 -0600

    panel admin updated

--- Data/Models/QuejaModel.cs
+++ Data/Models/QuejaModel.cs
@@ -7,7 +7,7 @@ namespace taanbus.Data.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "El motivo es un campo requerido")]
         public string? MotivoQueja { get; set; }
-        public DateTime FechaHechos { get; set; }
+        public DateTime? FechaHechos { get; set; }
         [Required(ErrorMessage = "La descripción es un campo requerido")]
         public string? Descripcion { get; set; }
         public DateTime FechaRegistro { get; set; }
