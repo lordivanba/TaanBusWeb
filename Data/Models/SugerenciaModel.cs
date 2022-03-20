87dcef6dc83d4df573bf109bb696a9bfa5d10b92
Ivan Canto <ivanbamail@gmail.com>
2022-03-19 19:20:11 -0600

    PageTitle updates

--- Data/Models/SugerenciaModel.cs
+++ Data/Models/SugerenciaModel.cs
@@ -7,7 +7,7 @@ namespace taanbus.Data.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "La descripción es un campo requerido")]
         public string? Descripcion { get; set; }
-        public DateTime? FechaRegistro { get; set; }
+        public DateTime FechaRegistro { get; set; }
         public int? Status { get; set; } = 0;
         public int? UserId { get; set; }
         public string? Ciudadano { get; set; }
